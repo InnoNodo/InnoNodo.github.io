1dd056234a151719c2a279e55d6543b10eb1ae4a
InnoNodo <k.nosov@innopolis.university>
2024-07-07 14:52:08 +0300

    Set up GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  basePath: process.env.NODE_ENV === 'production' ? '/InnoNodo.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/InnoNodo.github.io/' : '',
+};
