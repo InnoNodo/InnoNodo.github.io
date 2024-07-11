884d3fb6df885911aaba6eaf2ba65ca2bdd4e423
InnoNodo <k.nosov@innopolis.university>
2024-07-11 13:58:41 +0300

    Add head fir the pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,21 @@
 import '../index.css';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    );
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/landing-page.png" />
+        <title>Portfolio</title>
+        <meta name="author" content="Kirill Nosov" />
+        <meta name="description" content="Portfolio" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
